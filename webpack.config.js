f3dddaff92f9735982fad494f179660c1f5ed8d9
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-05-21 09:48:57 +0200

    save before merge

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = () => {
         module: {
             rules: [
                 {
-                    test: /\.(png|jpg|gif)$/,
+                    test: /\.(png|jpg|gif|svg)$/,
                     use: [
                         {
                             loader: "file-loader",
