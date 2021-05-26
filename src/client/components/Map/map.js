e8979a47ceb57090ee8c230bf03e1be89c52c0c1
letocart samuel <samuel.le.belgique@gmail.com>
2021-05-26 12:00:29 +0200

    parameter leaflet zoom

--- src/client/components/Map/map.js
+++ src/client/components/Map/map.js
@@ -13,10 +13,15 @@ const ViewMap = ({ data }) => {
 
     return (
         <div id="map">
-            <MapContainer center={[50.632557, 5.579666]} zoom={17} minZoom={15} scrollWheelZoom={true}>
+            <MapContainer center={[50.632557, 5.579666]} zoom={17} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='Map Data &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> Imagery  &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
                     url="https://api.mapbox.com/styles/v1/anonymouse97/ckp4eqckf01ij17ldyl6kz9iq/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5vbnltb3VzZTk3IiwiYSI6ImNrcDRlbjQ5YTA2MHoycHF0bDZ3OGwzd2UifQ.Kes9Z4RYBRVOboBqFs1kEw"
+
+                    minZoom={17}
+                    maxZoom={21}
+                    maxNativeZoom={18}
+
                 />
                 <LeafletCanvasMarker trees={data} />
             </MapContainer>
